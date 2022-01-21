b507a829ab80928e2d3c73fd1badc32b51cc6167
FrontDoor Dev <98183759+vjtairbnb1@users.noreply.github.com>
2022-01-21 15:05:40 -0500

    uploaded python functions

---
+++ python/sendInvoiceEmails/contact_info.py
@@ -0,0 +1,68 @@
+myContactInfo = {
+    'vinceTroyer': {
+        'firstName': 'Vincent',
+        'lastName': 'Troyer',
+        'email': 'vjt.airbnb@gmail.com',
+        'phone': '9417250447',
+        'address': '2212 Vintage St, Sarasota, FL 34240'
+    }
+}
+
+clientList = {
+    'firmanMiller': {
+        'firstName': 'Firman',
+        'lastName': 'Miller',
+        'email': 'cf2@emyprinter.net',
+        'phone': '3306744505',
+        'billingAddress': '5440 Township Rd 618, Millersburg, OH 44654',
+        'propertyAddress': '1020 S Conrad Ave, Sarasota, FL 34237'
+    },
+    'jesseMiller': {
+        'firstName': 'Jesse',
+        'lastName': 'Miller',
+        'email': 'jesse@thinkinkllc.com',
+        'phone': '3302432511',
+        'billingAddress': '8057 OH-241, Millersburg, OH 44654',
+        'propertyAddress': '972 Pattison Ave, Sarasota, FL 34237'
+    },
+    'jonasHochstetler': {
+        'firstName': 'Jonas',
+        'lastName': 'Hochstetler',
+        'email': 'sales@beechvalelaminating.com',
+        'phone': None,
+        'billingAddress': '7241 Township Rd 572, Millersburg, OH 44654',
+        'propertyAddress': '1202 Oak View Dr, Sarasota, FL 34232'
+    },
+    'leonTroyer': {
+        'firstName': 'Leon',
+        'lastName': 'Troyer',
+        'email': 'leon3433@yahoo.com',
+        'phone': '3304643433',
+        'billingAddress': '15258 Durstine Rd, Dundee, OH 44624',
+        'propertyAddress': '1202 Oak View Dr, Sarasota, FL 34232'
+    },
+    'lavonYoder': {
+        'firstName': 'Lavon',
+        'lastName': 'Yoder',
+        'email': 'leyoder02@gmail.com',
+        'phone': '5743044587',
+        'billingAddress': '6035 W 600 N, Shipshewana, IN 46565',
+        'propertyAddress': '229 E Cornelius Cir, Sarasota, FL 34232'
+    },
+    'lloydWittmer': {
+        'firstName': 'Lloyd',
+        'lastName': 'Wittmer',
+        'email': None,
+        'phone': '8122592267',
+        'billingAddress': '10949 E 950 N, Odon, IN 47562',
+        'propertyAddress': '4198 Eastwood Dr, Sarasota, FL 34232'
+    },
+    'scottYoder': {
+        'firstName': 'Scott',
+        'lastName': 'Yoder',
+        'email': 'scottyoder30@gmail.com',
+        'phone': '2698326643',
+        'billingAddress': '806 Ponder Ave, Sarasota, FL 34232',
+        'propertyAddress': '5150 Island Date St, Sarasota, FL 34232'
+    }
+}
---
+++ python/sendInvoiceEmails/to_pdf.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import pdfkit
+
+type_xlsx = pd.read_excel("")
