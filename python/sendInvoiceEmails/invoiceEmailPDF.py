935b72a114bb17d7dc714dbd6cb7a8422f1b3037
Vince <vincetroyer04@gmail.com>
2022-08-28 19:34:11 -0400

    Script msg change & New invoices
    - Removed the '***' lines from email

--- python/sendInvoiceEmails/invoiceEmailPDF.py
+++ python/sendInvoiceEmails/invoiceEmailPDF.py
@@ -17,14 +17,11 @@ lastMonth = (first - datetime.timedelta(days=25)).strftime("%B")
 # ------------ Build sendEmail fn() w/ email information ------------ #
 def sendEmail(receiverEmail, firstName, fileName): 
     alertMessage = "If you have any questions/concerns, please contact Vincent Troyer at vjt.airbnb@gmail.com or 941-725-0447. Thank you."
-    starString = "*" * (len(alertMessage) * 2)
 
     body = (f"Hi {firstName},\n\n"
         f"Attached to this email is a copy of your invoice for the month of {lastMonth}. Thank you for choosing us for your AirBnB management needs!\n\n"
-        f"{starString}\n"
         f"Please Note: This is an automated message. Please do not respond to this email and/or email address. "
         f"{alertMessage}\n"
-        f"{starString}\n\n"
         f"Vincent Troyer\n"
         f"FrontDoor Solutions\n"
         f"vjt.airbnb@gmail.com || 941-725-0447")
@@ -61,22 +58,22 @@ else:
             continue
 
 # ------------ Test Send to Myself ------------ #
-# sendEmail("vjt.airbnb@gmail.com", clientList["firmanMiller"]["firstName"], "fMiller")
-# sendEmail("vjt.airbnb@gmail.com", clientList["darylStoltzfus"]["firstName"], "dStoltzfus")
-# sendEmail("vjt.airbnb@gmail.com", clientList["jesseMiller"]["firstName"], "jMiller")
-# # sendEmail("vjt.airbnb@gmail.com", clientList["lavonYoder"]["firstName"], "lYoder")
-# sendEmail("vjt.airbnb@gmail.com", clientList["scottYoder"]["firstName"], "sYoder")
-# sendEmail("vjt.airbnb@gmail.com", clientList["leahBontrager"]["firstName"], "lBontrager")
-# sendEmail("vjt.airbnb@gmail.com", clientList["kateYoder"]["firstName"], "kYoder")
+sendEmail("vjt.airbnb@gmail.com", clientList["firmanMiller"]["firstName"], "fMiller")
+sendEmail("vjt.airbnb@gmail.com", clientList["darylStoltzfus"]["firstName"], "dStoltzfus")
+sendEmail("vjt.airbnb@gmail.com", clientList["jesseMiller"]["firstName"], "jMiller")
+# sendEmail("vjt.airbnb@gmail.com", clientList["lavonYoder"]["firstName"], "lYoder")
+sendEmail("vjt.airbnb@gmail.com", clientList["scottYoder"]["firstName"], "sYoder")
+sendEmail("vjt.airbnb@gmail.com", clientList["leahBontrager"]["firstName"], "lBontrager")
+sendEmail("vjt.airbnb@gmail.com", clientList["kateYoder"]["firstName"], "kYoder")
 
 
 # ------------ Send Email List ------------ #
-sendEmail(clientList["leahBontrager"]["email"], clientList["leahBontrager"]["firstName"], "lBontrager")
-# sendEmail(clientList["lavonYoder"]["email"], clientList["lavonYoder"]["firstName"], "lYoder")
-sendEmail(clientList["darylStoltzfus"]["email"], clientList["darylStoltzfus"]["firstName"], "dStoltzfus")
-sendEmail(clientList["scottYoder"]["email"], clientList["scottYoder"]["firstName"], "sYoder")
-sendEmail(clientList["kateYoder"]["email"], clientList["kateYoder"]["firstName"], "kYoder")
-sendEmail(clientList["jesseMiller"]["email"], clientList["jesseMiller"]["firstName"], "jMiller")
-sendEmail(clientList["firmanMiller"]["email"], clientList["firmanMiller"]["firstName"], "fMiller")
+# sendEmail(clientList["leahBontrager"]["email"], clientList["leahBontrager"]["firstName"], "lBontrager")
+# # sendEmail(clientList["lavonYoder"]["email"], clientList["lavonYoder"]["firstName"], "lYoder")
+# # sendEmail(clientList["darylStoltzfus"]["email"], clientList["darylStoltzfus"]["firstName"], "dStoltzfus")
+# sendEmail(clientList["scottYoder"]["email"], clientList["scottYoder"]["firstName"], "sYoder")
+# sendEmail(clientList["kateYoder"]["email"], clientList["kateYoder"]["firstName"], "kYoder")
+# # sendEmail(clientList["jesseMiller"]["email"], clientList["jesseMiller"]["firstName"], "jMiller")
+# sendEmail(clientList["firmanMiller"]["email"], clientList["firmanMiller"]["firstName"], "fMiller")
 
 # ------------ REMEMBER THAT THE MONTH IN THE EMAIL IS THE PREVIOUS MONTH RELATIVE TO THE CURRENT DATE (ex: January's invoice will be sent in February) ------------
